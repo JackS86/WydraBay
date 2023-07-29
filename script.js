b694214eecb39fc19510a877213532f49f97dec2
Jacek Stępień <jacekstepien6@gmail.com>
2023-07-29 19:19:26 +0200

    returned site

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log("skrypt dodany")
 let SELEKTOR_LARGE_PICTURE = '[data-kind-picture="goal"]';
 let SELEKTOR_TITLE_PICTURE = '[data-kind-picture="title"]';
 let SELEKTOR_MINIATURA_PICTURE = '[data-kind-picture="trigger"]';
-// let SELEKTOR_URL_PICTURE = "[data-url-picture]";
-// let SELEKTOR_TITLE_TWO_PICTURE = "[data-title-picture]";
+let CLASS_HIDE_LARGE_PICTURE = 'hide-large-image-responsive';
+const KEY_ESC = 27;
+
 
 let changePic = (urlPicture, textTittle) => {
     'use strict';
@@ -35,4 +36,36 @@ function addServiceClickByMiniatura(miniatura) {
     changePicByMiniatura(miniatura);       
     });
 }
+function readTableMiniatures() {
+    'use strict';
+    let miniatures = document.querySelectorAll(SELEKTOR_MINIATURA_PICTURE);
+    let tableMiniatures = [].slice.call(miniatures);
+    return tableMiniatures;
+}
+function hideLargeImage() {
+    'use strict';
+    document.body.classList.add(CLASS_HIDE_LARGE_PICTURE);
+}
+
+function addServiceKey() {
+    'use strict';
+    document.body.addEventListener('keyup', function(event) {
+        event.preventDefault();
+        if (event.keyCode === KEY_ESC) {
+            console.log(event.keyCode);
+            hideLargeImage();
+        }
+
+    });
+}
+
+function initializeEvents(){
+    'use strict';
+    let miniatures = readTableMiniatures();
+    miniatures.forEach(addServiceClickByMiniatura);
+    addServiceKey();
+}
+
+
+initializeEvents();
 
